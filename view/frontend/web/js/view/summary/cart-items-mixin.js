7ce08dcaaaf11fc285860d733f847f1bf7bcb7c6
Mark Shust <mark@shust.com>
2022-11-18 15:43:42 -0500

    Override an existing UI Component template

--- view/frontend/web/js/view/summary/cart-items-mixin.js
+++ view/frontend/web/js/view/summary/cart-items-mixin.js
@@ -3,6 +3,9 @@ define([], function() {
 
     return function (Component) {
         return Component.extend({
+            defaults: {
+                template: 'Macademy_CheckoutMessages/summary/cart-items'
+            },
             isItemsBlockExpanded: function() {
                 // If you wish to execute parent method, be sure to call
                 // this._super();
